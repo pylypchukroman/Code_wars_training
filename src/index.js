ff5552480060b87aa6ea03b105766562610b3787
pylypchukroman <romanpylypchuk@gmail.com>
2022-05-03 06:48:25 +0300

    last

--- src/index.js
+++ src/index.js
@@ -496,3 +496,23 @@ function reverse(str) {
 function createPhoneNumber(numbers) {
   return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
 }
+//Who likes it?
+function likes(names) {
+  if (names.length < 1) {
+    return `no one likes this`;
+  } else if (names.length == 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length == 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length == 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length >= 4) {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+//Find the unique number
+function findUniq(arr) {
+  return +arr.filter(value => {
+    return arr.indexOf(value) == arr.lastIndexOf(value);
+  });
+}
