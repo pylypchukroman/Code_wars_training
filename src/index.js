bdaffb71f820aa3c39401aba7e6dbf82c3268ce9
pylypchukroman <romanpylypchuk@gmail.com>
2022-06-07 08:26:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -822,4 +822,39 @@ function vowelIndices(word) {
   }
   return result;
 }
-console.log(vowelIndices('Apple'));
+// console.log(vowelIndices('Apple'));
+///
+function greetDevelopers(list) {
+  list.forEach(item => {
+    item.greeting = `Hi ${item.firstName}, what do you like the most about ${item.language}?`;
+  });
+  return list;
+}
+console.log(
+  greetDevelopers([
+    {
+      firstName: 'Sofia',
+      lastName: 'I.',
+      country: 'Argentina',
+      continent: 'Americas',
+      age: 35,
+      language: 'Java',
+    },
+    {
+      firstName: 'Lukas',
+      lastName: 'X.',
+      country: 'Croatia',
+      continent: 'Europe',
+      age: 35,
+      language: 'Python',
+    },
+    {
+      firstName: 'Madison',
+      lastName: 'U.',
+      country: 'United States',
+      continent: 'Americas',
+      age: 32,
+      language: 'Ruby',
+    },
+  ]),
+);
